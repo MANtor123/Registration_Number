c2bf44b6b5554cdf5879abc5c43fb0f0d77030bc
Mponeng <mantombiratego@gmail.com>
2017-03-17 10:29:57 +0200

    added files

---
+++ registration_number.js
@@ -0,0 +1,20 @@
+var btn = document.querySelector('#button');
+var box =document.querySelector('.text')
+
+
+
+btn.addEventListener('click', () => {
+
+if (box.value == ''){
+
+}
+else {
+  var newDiv = document.createElement('div')
+  var newContent = document.createTextNode('');
+  newDiv.appendChild(newContent);
+  newDiv.setAttribute('id','div1');
+  newDiv.innerHTML= box.value;
+  document.body.appendChild(newDiv);
+  var blank = document.querySelector('.text').value = '';
+}
+});
