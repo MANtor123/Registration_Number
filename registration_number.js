5409fd19d519c92c9c55eadd4ca118e9a3465b6d
Mponeng <mantombiratego@gmail.com>
2017-03-20 16:12:45 +0200

    made changes

--- registration_number.js
+++ registration_number.js
@@ -1,11 +1,17 @@
 var btn = document.querySelector('#button');
 var box =document.querySelector('.text');
+var list = document.querySelector('.lists')
+//var isValidReg = funtion (regNumber) {
+//return regNumber.startsWith ('CA')
+//|| regNumber.startsWith ('CJ')
+//|| regNumber.startsWith ('CY');
+//}
 
-var isValidReg = funtion (regNumber) {
-return regNumber.startsWith ('CA')
-|| regNumber.startsWith ('CJ') ||
-regNumber.startsWith ('CY');
-}
+list.addEventListener('change', function(evt){
+  var clicked = evt.target.value
+  
+
+});
 
 btn.addEventListener('click', function() {
 
@@ -21,4 +27,7 @@ else {
   document.body.appendChild(newDiv);
   var blank = document.querySelector('.text').value = '';
 }
+
+var li = document.createElement('li')
+li.innerHTML = newDiv;
 });
