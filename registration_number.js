72b9d24a8dc6859578381e014b0df1c66616629c
Mponeng <mantombiratego@gmail.com>
2017-03-23 14:16:03 +0200

    changed function

--- registration_number.js
+++ registration_number.js
@@ -1,33 +1,48 @@
 var btn = document.querySelector('#button');
 var box =document.querySelector('.text');
-var list = document.querySelector('.lists')
-//var isValidReg = funtion (regNumber) {
-//return regNumber.startsWith ('CA')
-//|| regNumber.startsWith ('CJ')
-//|| regNumber.startsWith ('CY');
-//}
-
-list.addEventListener('change', function(evt){
-  var clicked = evt.target.value
-  
-
-});
+var list = document.querySelector('.lists');
+var paragraph = document.getElementById('para')
+var array = [];
 
 btn.addEventListener('click', function() {
 
+array.push(box.value);
 if (box.value == ''){
 
 }
 else {
-  var newDiv = document.createElement('div')
-  var newContent = document.createTextNode('');
-  newDiv.appendChild(newContent);
-  newDiv.setAttribute('id','div1');
-  newDiv.innerHTML= box.value;
-  document.body.appendChild(newDiv);
+  var ul = document.createElement('li')
+  //var newContent = document.createTextNode('');
+  ul.textContent = box;
+  //ul.appendChild(li);
+  ul.setAttribute('id','div1');
+  ul.innerHTML= box.value;
+  document.body.appendChild(ul);
   var blank = document.querySelector('.text').value = '';
 }
 
-var li = document.createElement('li')
-li.innerHTML = newDiv;
+//var li = document.createElement('li')
+//li.innerHTML = newDiv;
 });
+
+list.addEventListener('change', function (e) {
+  var filterArr = [];
+    for (var i = 0; i < array.length; i++) {
+
+    if(e.target.value === 'default'){
+      array[i].style.display = 'block'
+
+    }
+
+    else if (array[i].startsWith(e.target.value)){
+      filterArr.push(array[i]);
+
+    }
+    else {
+      array[i].style = 'none';
+
+    }
+  }
+  
+  paragraph.innerHTML = filterArr;
+  });
